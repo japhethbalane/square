a0c6dceb77519ea8aa378fa7e4e3e14491316b26
Japheth Balane <japheth162@gmail.com>
2016-05-24 02:18:13 +0800

    Changed bg-style; added Object Square properties;

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ var squareSize = 50;
 var xborder = (canvas.width-(squareSize*(Math.floor(canvas.width/squareSize))))/2;
 var yborder = (canvas.height-(squareSize*(Math.floor(canvas.height/squareSize))))/2;
 
-
 //////////////////////////////////////////////////////////////////////////////
 
 setInterval(drawWorld, 20);
@@ -24,9 +23,9 @@ generateSquare(100);
 //////////////////////////////////////////////////////////////////////////////
 
 function generateSquare(count) {
-	// for (var i = 0; i < count; i++) {
-	// 	squares.push(new Square());
-	// };
+	for (var i = 0; i < count; i++) {
+		squares.push(new Square());
+	};
 }
 
 var mousePress = function(event) {
@@ -80,7 +79,9 @@ function drawWorld() {
 		drawSquareLogo();
 	};
 	if (isPlaying) {
-		
+		for (var i = 0; i < squares.length; i++) {
+			squares[i].update().draw();
+		}
 	};
 }
 
@@ -139,13 +140,22 @@ function drawSquareLogo() {
 }
 
 function clearCanvas() {
-	context.fillStyle = "#000";
-	context.fillRect(0, 0, canvas.width, canvas.height);
+	gradient = context.createRadialGradient(
+		canvas.width/2,canvas.height/2,5,canvas.width/2,canvas.height/2,800);
+	gradient.addColorStop(0,'#000');
+	gradient.addColorStop(0.1,"#000");
+	gradient.addColorStop(0.2,'#004');
+	gradient.addColorStop(0.35,'#000');
+	gradient.addColorStop(0.5,'#002');
+	gradient.addColorStop(0.75,'#000');
+	gradient.addColorStop(1,'#004');
+	context.fillStyle = gradient;
+	context.fillRect(0,0,canvas.width,canvas.height);
 }
 
 function drawGrid() {
 
-	context.strokeStyle = "#666";
+	context.strokeStyle = "#000";
 
 	context.moveTo(xborder,yborder);
 	context.lineTo(canvas.width-xborder,yborder);
@@ -178,7 +188,21 @@ function randomBetween(min, max) {
 //////////////////////////////////////////////////////////////////////////////
 
 function Square() {
-	
+	this.x = squareSize*randomBetween(0,Math.floor(canvas.width/squareSize))+xborder;
+	this.y = squareSize*randomBetween(0,Math.floor(canvas.height/squareSize))+yborder;
+	this.dimention = squareSize-10;
+
+	this.update = function() {
+		
+		return this;
+	}
+
+	this.draw = function() {
+		context.beginPath();
+		context.fillStyle = "rgba(255,255,255,0.3)";
+		context.fillRect(this.x+5,this.y+5,this.dimention,this.dimention);
+		context.fill();
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////
